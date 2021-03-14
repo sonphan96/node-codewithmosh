52cbeef64a5e1cc67e6b6b528f7f4a9608f387cf
johnson <phantruongson96@gmail.com>
2021-03-14 03:23:54 -0400

    CRUD operation

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -29,16 +29,66 @@ async function createCourse() {
 };
 
 async function getCourses() {
+  // Comparison Query
+  // .find({ price: { $gte: 10, $lte: 20 }})
+  // .find({ price: { $in: [10, 15, 20] }})
+
+  // Logical operator
+  // .find()
+  // .or([{author: 'Mosh'}, {isPublished: true}])
+  // .and([{}, {}])
+
+  // .find({ author: /^Mosh/ })   // /pattern/
+  // .find({ author: /Hame$/i})
+  // .find({ author: /.*Mosh.*/})
+  const pageNumber = 2;
+  const pageSize = 10;
+
   const result = await Course
     .find( { author: 'Mosh', isPublished: true })
-    // Comparition Query
-    // .find({ price: { $gte: 10, $lte: 20 }})
-    // .find({ price: { $in: [10, 15, 20] }})
-    .limit(10)
+    .skip((pageNumber -1)*pageSize)
+    .limit(pageSize)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1});
+    // .select({ name: 1, tags: 1});
+    .count();
     
   console.log(result);
 }
 
-getCourses();
+async function updateCourse(id) {
+  // Query first
+  // findByID
+  // const course = await Course.findById(id);
+  // modify
+  // if(!course) return;
+
+  // course.isPublished = true;
+  // course.author = 'Another author';
+  // course.set({
+  //   isPublished: true,
+  //   author: 'Another Author'
+  // })
+
+  // save
+  // const result = await course.save();
+  // console.log(result);
+
+  // update first
+  // update directly
+  const result = await Course.find({ _id: id }, {
+    $set: {
+      author: 'Mosh',
+      isPublished: false
+    }
+  });
+  // Optional: get update document
+  console.log(result);
+
+}
+
+async function removeCourse(id) {
+  const result = await Course.deleteOne({ _id: id });
+
+}
+
+updateCourse('604d9511faff3b408999fcb0');
